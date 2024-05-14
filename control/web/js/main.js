30d0b96944ded1cc8a165cf8455591c29f8520c8
AbisiaMera <122136750+AbisiaMera@users.noreply.github.com>
2024-05-14 12:38:10 -0600

    Add files via upload

---
+++ control/web/js/main.js
@@ -0,0 +1,45 @@
+//alert("Hola mundo");
+let Adelante=document.getElementById("Adelante");
+let Atras=document.getElementById("Atras");
+let Derecha=document.getElementById("Derecha");
+let Izquierda=document.getElementById("Izquierda");
+let Detener=document.getElementById("Detener");
+let Mensaje=document.getElementById("Mensaje");
+
+function CallApi(estatus)
+{
+    // Hacer una petición para un usuario con ID especifico
+    axios.get('http://localhost/iot-car-control/back-end/apis/setRegistro.php?valorEstatus='+estatus)
+    .then(function (response) {
+    // manejar respuesta exitosa
+    console.log(response);
+    Mensaje.innerHTML="Respuesta: <strong>"+response.data+"</strong>";
+    })
+    .catch(function (error) {
+    // manejar error
+    console.log(error);
+    })
+    .finally(function () {
+    // siempre sera executado
+    });
+}
+
+Adelante.addEventListener("click",function(){
+    CallApi('w');
+});
+
+Atras.addEventListener("click",function(){
+    CallApi('s');
+});
+
+Derecha.addEventListener("click",function(){
+    CallApi('d');
+});
+
+Izquierda.addEventListener("click",function(){
+    CallApi('a');
+});
+
+Detener.addEventListener("click",function(){
+    CallApi('P');
+});
