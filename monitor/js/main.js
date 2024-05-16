5cdd4d17c23adc318bcb541fc7934909705ff7b0
AbisiaMera <122136750+AbisiaMera@users.noreply.github.com>
2024-05-16 11:43:18 -0600

    Add files via upload

---
+++ monitor/js/main.js
@@ -0,0 +1,31 @@
+//alert("Hola mundo");
+
+let Respuesta=document.getElementById("Respuesta");
+
+function CallApiRequest()
+{
+    // Hacer una petición para un usuario con ID especifico
+    axios.get('http://localhost/iot-car-control/back-end/apis/getRegistro.php?valorEstatus=')
+    .then(function (response) {
+    // manejar respuesta exitosa
+    console.log(response);
+
+    let RespuestaServer = 
+    response.data == "w"?"Adelante":
+    response.data == "s"?"Atras":
+    response.data == "d"?"Derecha":
+    response.data == "a"?"Izquierda":
+    "Detener";
+
+    Respuesta.innerHTML="Respuesta: <strong>"+RespuestaServer+"</strong>";
+    })
+    .catch(function (error) {
+    // manejar error
+    console.log(error);
+    })
+    .finally(function () {
+    // siempre sera executado
+    });
+}
+
+setInterval(CallApiRequest, 2000);
